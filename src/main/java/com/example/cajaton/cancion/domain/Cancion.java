a973b497eb75f376c0dadcb403444af35c3a9838
Luis23345432 <luis.lopez.c@utec.edu.pe>
2024-06-01 15:31:01 -0500

    aa

--- src/main/java/com/example/cajaton/cancion/domain/Cancion.java
+++ src/main/java/com/example/cajaton/cancion/domain/Cancion.java
@@ -1,4 +1,37 @@
 package com.example.cajaton.cancion.domain;
 
+import com.example.cajaton.album.domain.Album;
+import com.example.cajaton.artista.domain.Artista;
+import com.example.cajaton.listareproduccion.domain.ListaReproduccion;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+
+@Entity
+@Table(name = "canciones")
+@Data
 public class Cancion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idSong;
+
+    private String titulo;
+
+    private Integer duracion;
+
+    @ManyToOne
+    @JoinColumn(name = "artistaID")
+    private Artista artistaID;
+
+    @ManyToOne
+    @JoinColumn(name = "album")
+    private Album idAlbum;
+
+    @ManyToOne
+    @JoinColumn(name = "ListaDeReproduccion")
+    private ListaReproduccion idListaReproduccion;
 }
+
